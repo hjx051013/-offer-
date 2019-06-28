47dfa68a50adf9c71d0472650effa1a29b8435c8
Jersey Huang <1830488060@qq.com>
2019-06-28 16:56:33 +0800

    Update FindSubString.java

--- src/com/leetcode/FindSubString.java
+++ src/com/leetcode/FindSubString.java
@@ -1,11 +1,12 @@
+package com.leetcode;
+
 import java.util.*;
 
 public class FindSubString {
     public static void main(String[] args) {
-        System.out.println("hello world");
         Solution64 s = new Solution64();
-        String[] words = {"foo","bar"};
-        String str = "barfoothefoobarman";
+        String[] words = {"aa","aa","aa"};
+        String str = "aaaaaaaa";
         List<Integer> res = s.findSubstring(str, words);
         for(Integer e:res) {
             System.out.print(e+" ");
@@ -13,67 +14,45 @@ public class FindSubString {
     }
 
 }
+
 class Solution64 {
     public List<Integer> findSubstring(String s, String[] words) {
-        // Set<String> set = new HashSet<>();
         List<Integer> res = new ArrayList<>();
-        Map<String,Integer> map= new HashMap<>();
-        for(String word: words) {
-            if(map.containsKey(word)) map.put(word, map.get(word)+1);
-            else map.put(word,1);
+        if(s.length()==0 || words.length==0) return res;
+        Map<String, Integer> map = new HashMap<>();
+        int totLen  = words.length*words[0].length();
+        for(int i = 0; i < words.length; i++) {
+            if(map.containsKey(words[i])) map.put(words[i], map.get(words[i])+1);
+            else map.put(words[i], 1);
         }
-        List<Pair<Integer,String>> list = new ArrayList<>();
-        for(String word:map.keySet()) {
-            int start = 0;
-            if(s.indexOf(word) < 0) return res;
-            while(start<s.length()&&(start = s.substring(start).indexOf(word)) > 0) {
-                list.add(new Pair<Integer,String>(start,word));
-                start += word.length();
-            }
-        }
-        
-        list.sort(new Comparator<Pair<Integer,String>>() {
-            public int compare(Pair<Integer,String> p1, Pair<Integer,String> p2) {
-                if(p1.val1 < p2.val1) return -1;
-                else if(p1.val1 == p2.val1) return 0;
-                else return 1;
-            }
-        });
 
-        Map<String, Integer> example = new HashMap<>();
-        for(int i = 0; i <= list.size()-words.length; i++) {
-            boolean valid = isValid(map,list,i,words.length);
-            if(valid) res.add(list.get(i).val1);
+        for(int i = 0; i <= s.length()-totLen; i++) {
+            if(isValid(s.substring(i), map, words[0].length(), words.length)) res.add(i);
         }
 
         return res;
     }
 
-    private boolean isValid(Map<String, Integer> map, List<Pair<Integer,String>> list, int index, int num) {
-        Map<String, Integer> strCnt = new HashMap<>();
-        for(int i = 0; i < index+num; i++) {
-            Pair<Integer,String> p = list.get(i);
-            if(strCnt.containsKey(p.val2)) {
-                strCnt.put(p.val2,strCnt.get(p.val2)+1);
-            } else {
-                strCnt.put(p.val2,1);
-            }
-        }
-        for(String str:map.keySet()) {
-            if(!strCnt.containsKey(str)) return false;
-            if(strCnt.get(str)!=map.get(str)) return false;
+    private boolean isSame(Map<String,Integer> strMap, Map<String,Integer> wordMap) {
+        if(strMap.keySet().size()!=wordMap.keySet().size()) return false;
+        for(String key:wordMap.keySet()) {
+            if(!wordMap.containsKey(key)) return false;
+            if(wordMap.get(key)!=strMap.get(key)) return false;
         }
 
         return true;
     }
-}
+    private boolean isValid(String str, Map<String,Integer> wordMap, int wordLen, int wordNum) {
+        Map<String,Integer> strMap = new HashMap<>();
 
-class Pair<T1,T2>
-{
-    public T1 val1;
-    public T2 val2;
-    public Pair(T1 v1, T2 v2) {
-        val1 = v1;
-        val2 = v2;
+        for(int i = 0; i < wordNum; i++) {
+            String newWord = str.substring(i*wordLen,(i+1)*wordLen);
+            if(strMap.containsKey(newWord)) {
+                strMap.put(newWord,strMap.get(newWord)+1);
+            } else {
+                strMap.put(newWord,1);
+            }
+        }
+
+        return isSame(strMap,wordMap);
     }
-}
